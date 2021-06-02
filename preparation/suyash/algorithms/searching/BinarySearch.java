2ac233344a29d2d76dc915d5a99e2c25dedebd3c
sms0070 <suyash.sms@gmail.com>
2021-06-02 22:21:31 +0530

    Binary Search - complete
    Signed-off-by: suyash mani sharma <suyash.sharma@sap.com>

--- preparation/suyash/algorithms/searching/BinarySearch.java
+++ preparation/suyash/algorithms/searching/BinarySearch.java
@@ -23,7 +23,7 @@ public class BinarySearch {
             }
         }
         int searchValue = in.nextInt();
-        int position = binary_searcher(numList, searchValue);
+        int position = binary_searcher(numList, searchValue, 0, numList.size() - 1, -1);
 
         if (position == -1) {
             System.out.println(searchValue + " not found");
@@ -32,9 +32,18 @@ public class BinarySearch {
         }
     }
 
-    private static int binary_searcher(List<Integer> numList, int searchValue) {
-        int position = -1;
+    private static int binary_searcher(List<Integer> numList, int searchValue, int l, int r, int pos) {
 
-        return position;
+
+        if (l > r) return pos;
+        if (numList.get((r + l) / 2) == searchValue) {
+            pos = (r + l) / 2;
+            return pos;
+        } else if (numList.get((r + l) / 2) < searchValue) {
+            return binary_searcher(numList, searchValue, (r + l) / 2 + 1, r, pos);
+        } else {
+            return binary_searcher(numList, searchValue, l, ((r + l) / 2) - 1, pos);
+        }
+
     }
 }
